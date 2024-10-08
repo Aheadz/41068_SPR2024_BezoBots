ef311ad08c82327ccee8cd07bc363b883e8a29e7
13366378 <zahead.rashidi@student.uts.edu.au>
2024-10-08 21:57:02 +1100

    automated cartographer launch

--- BezoBots_Main/launch/Sprint3/sprint3_slo_3_2.py
+++ BezoBots_Main/launch/Sprint3/sprint3_slo_3_2.py
@@ -31,6 +31,7 @@ def evaluate_joystick(context, *args, **kwargs):
         return []
 
 def generate_launch_description():   
+    # Declare arguments
     joystick_launch_arg = DeclareLaunchArgument(
         'joystick',
         default_value='False',
@@ -40,24 +41,27 @@ def generate_launch_description():
     # Get the path to your package's share directory
     package_share_dir = get_package_share_directory('bezobots')  # Replace with your package name
     
-
     # Declare the launch argument for selecting the turtlebot3_gazebo launch file
     turtlebot_launch_arg = DeclareLaunchArgument(
         'turtlebot_launch_file', 
         default_value='aws_world.launch.py',  # Default to this launch file
         description='Specify the TurtleBot3 Gazebo launch file to use'
     )
+
     # Launch the turtlebot3_gazebo with the selected launch file
     turtlebot3_gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('bezobots'), 'launch/'),
+            package_share_dir, 'launch/'),
             LaunchConfiguration('turtlebot_launch_file')])
     )
     
+    workspace_dir = os.getenv('COLCON_PREFIX_PATH', os.getcwd()).replace('/install','')
+    map_file_path = os.path.join(workspace_dir, 'src', '41068_SPR2024_BezoBots', 'BezoBots_Main', 'maps', 'gazebo_map', 'map.yaml')
+
     # Declare the map file path argument
     map_yaml_arg = DeclareLaunchArgument(
         'map_yaml_file',
-        default_value=os.path.join(get_package_share_directory('bezobots'), 'maps', 'gazebo_map', 'map.yaml'),
+        default_value=map_file_path,
         description='Full path to map file to load'
     )
 
@@ -69,49 +73,39 @@ def generate_launch_description():
             executable='map_server',
             name='map_server',
             output='screen',
-            parameters=[{'yaml_filename': '/home/strangelove/robo1_ws/src/41068_SPR2024_BezoBots/BezoBots_Main/maps/gazebo_map/map.yaml'}],  # Use the launch argument for map file
+            parameters=[{'yaml_filename': LaunchConfiguration('map_yaml_file'),  # Use the launch argument for map file
+                         'frame_id': 'odom'}],
             remappings=[('/map', '/gazebomap')]
         )]
     )
 
     # Delay lifecycle manager to give the map server time to start
     lifecycle_manager_cmd = TimerAction(
-        period=10.0,  # Delay for 7 seconds (adjust as needed, depending on how long the map_server takes)
+        period=10.0,  # Delay for 10 seconds (adjust as needed, depending on how long the map_server takes)
         actions=[Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager_localization',
             output='screen',
             parameters=[{
-                'use_sim_time': False,  # Adjust for simulation time if necessary
+                'use_sim_time': True,  # Adjust for simulation time if necessary
                 'autostart': True,  # Automatically configure and activate lifecycle nodes
                 'node_names': ['map_server']
             }]
         )]
     )
-
-    # Static Transform Publisher delayed accordingly
-    static_transform_cmd = TimerAction(
-        period=1.0,  # Delay for 9 seconds to ensure everything is initialized
-        actions=[Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            arguments=['0', '0', '0', '0', '0', '0', '1', 'gazebomap', 'odom'],  # Translation and rotation from gazebomap to odom
-            output='screen'
-        )]
-    )
-
+    
     rviz_config_dir = os.path.join(
         package_share_dir,
         'config',
-        'turtlebot_cartographer_merged.rviz'
+        'rviz_config_SLO_3_2.rviz'
     )
     
-    # rviz_config_dir = os.path.join(
-    #     get_package_share_directory('turtlebot3_description'),
-    #     'rviz',
-    #     'model.rviz')
+    cartographer_launch_arg = DeclareLaunchArgument(
+        'cartographer_launch_file', 
+        default_value='cartographer.launch.py',  # Default to this launch file
+        description='Specify the launch file that starts cartographer'
+    )
     
     rviz_launch = Node(
         package='rviz2',
@@ -124,10 +118,11 @@ def generate_launch_description():
     return LaunchDescription([
         turtlebot_launch_arg,  # Include the argument for selecting the TurtleBot3 Gazebo launch file
         joystick_launch_arg,  # Joystick argument
+        map_yaml_arg,  # Pass the map YAML argument
         turtlebot3_gazebo_launch,
         OpaqueFunction(function=evaluate_joystick),  # Evaluate joystick configuration
         rviz_launch,  # Launch RViz first
-        #map_server_cmd,  # Delayed map server launch
-        #lifecycle_manager_cmd,  # Delayed lifecycle manager launch
-        static_transform_cmd,  # Delayed static transform
+        map_server_cmd,  # Delayed map server launch
+        lifecycle_manager_cmd,  # Delayed lifecycle manager launch
+        cartographer_launch_arg,  # Pass the cartographer launch argument
     ])
--- BezoBots_Main/src/main.cpp
+++ BezoBots_Main/src/main.cpp
@@ -1,12 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include <cstdlib>
 #include "robo_s2.hpp"
+#include "robo_s3.hpp"
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   //auto node = std::make_shared<Ros2Lab2Node>(n);
-  auto node = std::make_shared<Sprint2>();
+  //auto node = std::make_shared<Sprint2>();
+  auto node = std::make_shared<Sprint3>();
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
